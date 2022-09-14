5ac290af5b4685917104effd85bd11ccbce70312
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2022-09-14 02:20:21 -0700

    fixed issue of all songs sharing the same chords

--- utility.py
+++ utility.py
@@ -1,20 +1,21 @@
 # A script that modifies the chord graph
 
 class Song:
-    def __init__(self, id: str='', chords: dict[str,str]=dict) -> None:
+    def __init__(self, id: str='', chords: dict[str,list[str]]=dict()) -> None:
         self.id: str = id
-        self.chords: dict[str,str] = chords
+        self.chords: dict[str,list[str]] = chords
 
-    def populate(self, new_id: str, new_chords: dict[str,str]) -> None:
+    def populate(self, new_id: str, new_chords: dict[str,list[str]]) -> None:
         self.id = new_id
         self.chords = new_chords
 
-    def info(self) -> tuple[str, dict[str,str]]:
+    def info(self) -> tuple[str, dict[str,list[str]]]:
         return (self.id, self.chords)
 
 class Chord_Graph:
     def __init__(self) -> None:
         self.songs: list[Song] = list()
+        self.graph: dict[str,list[str]] = dict()
 
     def load(self, path: str='README.md') -> None:
         with open(path) as file:
@@ -28,15 +29,25 @@ class Chord_Graph:
                 line = line.lstrip()
                 if line.startswith('%%'):
                     id = line[3:]
-                    self.songs.append(Song(id))
+                    new_song = Song(id, dict())
                     print(f'{id=}')
 
                 elif line == '':
-                    continue
+                    self.songs.append(new_song)
                 
                 # TODO: replace the chord search with a regex
                 else:
-                    print(line)
+                    # convert starting chord line to normal line
+                    if '((' in line:
+                        paren_start_idx = line.find('((')
+                        paren_end_idx = line[paren_start_idx:].find('))')
+                        line = line[:paren_start_idx] + line[paren_start_idx+paren_end_idx+2:]
+                    chords = line.split('-->')
+                    print(chords)
+                    # if chords[0] not in new_song.chords:
+                    new_song.chords[chords[0]] = chords[1]
+                        
+                    
 
 graph = Chord_Graph()
 graph.load()
