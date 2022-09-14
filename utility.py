fa8788d9f06c48be87d070e968e8e4ca029f289a
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2022-09-14 10:48:39 -0700

    removing ChordGraph class for now. Really overshoots the original plan of this script.

--- utility.py
+++ utility.py
@@ -12,46 +12,46 @@ class Song:
     def info(self) -> tuple[str, dict[str,list[str]]]:
         return (self.id, self.chords)
 
-class Chord_Graph:
-    def __init__(self) -> None:
-        self.songs: list[Song] = list()
-        self.graph: dict[str,list[str]] = dict()
+# class Chord_Graph:
+#     '''Load all existing songs into memory. This can let the user modify 
+#     individual chord changes. 
+#     '''
+#     def __init__(self) -> None:
+#         self.songs: list[Song] = list()
+#         self.graph: dict[str,list[str]] = dict()
 
-    def load(self, path: str='README.md') -> None:
-        with open(path) as file:
-            data = file.read()
-            # TODO: replace these two lines with a regex
-            start_idx = data.find('```mermaid')
-            end_idx = data[start_idx:].find('```', 3)
+#     def load(self, path: str='README.md') -> None:
+#         with open(path) as file:
+#             data = file.read()
+#             # TODO: replace these two lines with a regex
+#             start_idx = data.find('```mermaid')
+#             end_idx = data[start_idx:].find('```', 3)
 
-            for line in data[start_idx:start_idx+end_idx].split('\n')[2:]:
-                # TODO: replace the song artist and title search with a regex
-                line = line.lstrip()
-                if line.startswith('%%'):
-                    id = line[3:]
-                    new_song = Song(id, dict())
-                    print(f'{id=}')
+#             for line in data[start_idx:start_idx+end_idx].split('\n')[2:]:
+#                 # TODO: replace the song artist and title search with a regex
+#                 line = line.lstrip()
+#                 if line.startswith('%%'):
+#                     id = line[3:]
+#                     new_song = Song(id, dict())
+#                     print(f'{id=}')
 
-                elif line == '':
-                    self.songs.append(new_song)
+#                 elif line == '':
+#                     self.songs.append(new_song)
                 
-                # TODO: replace the chord search with a regex
-                else:
-                    # convert starting chord line to normal line
-                    if '((' in line:
-                        paren_start_idx = line.find('((')
-                        paren_end_idx = line[paren_start_idx:].find('))')
-                        line = line[:paren_start_idx] + line[paren_start_idx+paren_end_idx+2:]
-                    chords = line.split('-->')
-                    print(chords)
-                    if chords[0] not in new_song.chords:
-                        new_song.chords[chords[0]] = [chords[1]]
-                    else:
-                        new_song.chords[chords[0]] = 
-        # why do i need to build a model of the songs in memory? why can't i just add and remove songs to the README.md? by following the format of the file? delete songs by finding their song id (list all songs available to delete)
+#                 # TODO: replace the chord search with a regex
+#                 else:
+#                     # convert starting chord line to normal line
+#                     if '((' in line:
+#                         paren_start_idx = line.find('((')
+#                         paren_end_idx = line[paren_start_idx:].find('))')
+#                         line = line[:paren_start_idx] + line[paren_start_idx+paren_end_idx+2:]
+#                     chords = line.split('-->')
+#                     print(chords)
+#                     if chords[0] not in new_song.chords:
+#                         new_song.chords[chords[0]] = [chords[1]]
+#                     else:
+#                         new_song.chords[chords[0]] = 
+#         # why do i need to build a model of the songs in memory? why can't i just add and remove songs to the README.md? by following the format of the file? delete songs by finding their song id (list all songs available to delete)
                         
                     
-
-graph = Chord_Graph()
-graph.load()
 pass
