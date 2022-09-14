a26553c64d4a4ef5c9b8ecaaec34d52796f5356d
Samuel Edwards-Marsh <sam.david.marsh@gmail.com>
2022-09-14 11:34:40 -0700

    songs are loaded into the chord graph correctly

--- utility.py
+++ utility.py
@@ -1,9 +1,17 @@
 # A script that modifies the chord graph
 
 class Song:
+    '''Represents a song. 
+    '''
     def __init__(self, id: str='', chords: dict[str,list[str]]=dict()) -> None:
         self.id: str = id
         self.chords: dict[str,list[str]] = chords
+
+    def __str__(self) -> str:
+        s = f'{self.id}\n'
+        for chord in self.chords:
+            s += f'{chord}-->{self.chords[chord]}\n'
+        return s
 
     def populate(self, new_id: str, new_chords: dict[str,list[str]]) -> None:
         self.id = new_id
@@ -12,46 +20,53 @@ class Song:
     def info(self) -> tuple[str, dict[str,list[str]]]:
         return (self.id, self.chords)
 
-# class Chord_Graph:
-#     '''Load all existing songs into memory. This can let the user modify 
-#     individual chord changes. 
-#     '''
-#     def __init__(self) -> None:
-#         self.songs: list[Song] = list()
-#         self.graph: dict[str,list[str]] = dict()
+class Chord_Graph:
+    '''Represents all the songs from the chord graph.
+    '''
+    def __init__(self) -> None:
+        self.songs: list[Song] = list()
+        self.graph: dict[str,list[str]] = dict()
 
-#     def load(self, path: str='README.md') -> None:
-#         with open(path) as file:
-#             data = file.read()
-#             # TODO: replace these two lines with a regex
-#             start_idx = data.find('```mermaid')
-#             end_idx = data[start_idx:].find('```', 3)
+    def load(self, path: str='README.md') -> None:
+        with open(path) as file:
+            data = file.read()
+            # TODO: replace these two lines with a regex
+            start_idx = data.find('```mermaid')
+            end_idx = data[start_idx:].find('```', 3)
 
-#             for line in data[start_idx:start_idx+end_idx].split('\n')[2:]:
-#                 # TODO: replace the song artist and title search with a regex
-#                 line = line.lstrip()
-#                 if line.startswith('%%'):
-#                     id = line[3:]
-#                     new_song = Song(id, dict())
-#                     print(f'{id=}')
+            for line in data[start_idx:start_idx+end_idx].split('\n')[2:]:
+                # TODO: replace the song artist and title search with a regex
+                line = line.lstrip()
+                if line.startswith('%%'):
+                    id = line[3:]
+                    new_song = Song(id, dict())
+                    print(f'{id=}')
 
-#                 elif line == '':
-#                     self.songs.append(new_song)
+                elif line == '':
+                    self.songs.append(new_song)
                 
-#                 # TODO: replace the chord search with a regex
-#                 else:
-#                     # convert starting chord line to normal line
-#                     if '((' in line:
-#                         paren_start_idx = line.find('((')
-#                         paren_end_idx = line[paren_start_idx:].find('))')
-#                         line = line[:paren_start_idx] + line[paren_start_idx+paren_end_idx+2:]
-#                     chords = line.split('-->')
-#                     print(chords)
-#                     if chords[0] not in new_song.chords:
-#                         new_song.chords[chords[0]] = [chords[1]]
-#                     else:
-#                         new_song.chords[chords[0]] = 
-#         # why do i need to build a model of the songs in memory? why can't i just add and remove songs to the README.md? by following the format of the file? delete songs by finding their song id (list all songs available to delete)
-                        
-                    
+                # TODO: replace the chord search with a regex
+                else:
+                    # convert starting chord line to normal line
+                    if '((' in line:
+                        paren_start_idx = line.find('((')
+                        paren_end_idx = line[paren_start_idx:].find('))')
+                        line = line[:paren_start_idx] + line[paren_start_idx+paren_end_idx+2:]
+                    chords = line.split('-->')
+                    if chords[0] not in new_song.chords:
+                        new_song.chords[chords[0]] = [chords[1]]
+                    # don't add duplicate changes
+                    elif chords[1] in new_song.chords[chords[0]]:
+                        continue
+                    else:
+                        new_song.chords[chords[0]].append(chords[1])
+
+    # def add(self) -> None:
+    #     '''Add a song to the Chord Graph.
+    #     '''
+
+
+graph = Chord_Graph()
+graph.load()
+print(graph.songs[1])
 pass
